d62b4bb524a9941a6c777b80afdee630607c3cc0
ThilinaManamgoda <maanafunedu@gmail.com>
2018-11-13 17:45:27 +0530

    Change curl endpoint construct

--- ei_scenario_test.sh
+++ ei_scenario_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LOCALHOST=${TEST_URL}
-CODE=`wget --server-response "${LOCALHOST}" 2>&1 | awk '/^  HTTP/{print $2}'`;
+CODE=`wget --server-response "${LOCALHOST}/helloworld?name=Tom" 2>&1 | awk '/^  HTTP/{print $2}'`;
 if [ ${CODE} -eq "200" ] ; then
   echo "Test passed successfully..."
   exit 0
