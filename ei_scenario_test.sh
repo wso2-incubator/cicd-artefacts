7e52a4d6a47d23dee17595e047d06732379c6bce
nishadi <ndimeshi@gmail.com>
2018-11-13 11:25:16 +0530

    Add additional logs

--- ei_scenario_test.sh
+++ ei_scenario_test.sh
@@ -2,7 +2,9 @@
 LOCALHOST=${TEST_URL}
 CODE=`wget --server-response ${LOCALHOST}"/services/EchoProxy" 2>&1 | awk '/^  HTTP/{print $2}'`;
 if [ ${CODE} -eq "200" ] ; then
+  echo "Test passed successfully..."
   exit 0
 else
+  echo "Test failed..."
   exit 1
 fi
