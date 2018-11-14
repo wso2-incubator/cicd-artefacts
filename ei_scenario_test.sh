d4072eefb8c0f7d0ce10f9348f32726feb077158
KavinduZoysa <kavindu175@gmail.com>
2018-11-14 12:51:57 +0530

    Update ei_scenario_test.sh

--- ei_scenario_test.sh
+++ ei_scenario_test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 LOCALHOST=${TEST_URL}
+echo "Running test against the endpoint: ${TEST_URL}" 
 CODE=`wget --server-response "${LOCALHOST}/helloworld?name=Tom" 2>&1 | awk '/^  HTTP/{print $2}'`;
+echo "Status code: $CODE"
 if [ ${CODE} -eq "200" ] ; then
   echo "Test passed successfully..."
   exit 0
